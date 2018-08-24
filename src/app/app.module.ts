b9c2779c65bb07b5550752be86559f15bef852b9
Anousone Kaseumsouk <anousonekaseumsouk@gmail.com>
2018-08-24 10:24:13 -0700

    Add conentto promo-area.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule }  from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 
+
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { IphoneXComponent } from './iphoneX/iphoneX.component';

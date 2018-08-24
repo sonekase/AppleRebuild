342442881fcbee06609fac1c9475bdf1426754f3
Anousone Kaseumsouk <anousonekaseumsouk@gmail.com>
2018-08-24 12:52:57 -0700

    Add dynamic routing for appleWatch to nav.

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,6 +4,7 @@ import { WelcomeComponent } from './welcome/welcome.component';
 import { MacComponent } from './mac/mac.component';
 import { IpadComponent } from './ipad/ipad.component';
 import { IphoneXComponent } from './iphoneX/iphoneX.component';
+import { AppleWatchComponent } from './apple-watch/apple-watch.component';
 import { PromotionSectionComponent } from './promotion-section/promotion-section.component';
 import { PromoTwoComponent } from './promo-two/promo-two.component';
 import { PromoThreeComponent } from './promo-three/promo-three.component';
@@ -24,6 +25,10 @@ const appRoutes: Routes = [
   {
     path: 'iphoneX',
     component: IphoneXComponent
+  },
+  {
+    path: 'apple-watch',
+    component: AppleWatchComponent
   },
   {
     path: 'promotion-section',
